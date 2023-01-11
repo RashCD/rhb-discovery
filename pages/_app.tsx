177d14bfb7e626974231fdf3dfc8f855628faa3e
RashCD <mrrash91@gmail.com>
2023-01-12 02:37:36 +0800

    add default template for index page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { createTheme, ThemeProvider } from '@mui/material';
+import { Inter } from '@next/font/google';
+import Head from 'next/head';
 
 const theme = createTheme({
 	typography: {
@@ -8,10 +10,23 @@ const theme = createTheme({
 	},
 });
 
+const inter = Inter({ subsets: ['latin'] });
+
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<ThemeProvider theme={theme}>
-			<Component {...pageProps} />
+			<Head>
+				<title>RHB Connect</title>
+				<meta
+					name="description"
+					content="RHB connect make it easy for you to contact us for information regarding financial advices or services"
+				/>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<main className={inter.className}>
+				<Component {...pageProps} />
+			</main>
 		</ThemeProvider>
 	);
 }
