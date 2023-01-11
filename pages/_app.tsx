0ac13eab360e3dc45c0eb306c4e2b61167331b1c
RashCD <mrrash91@gmail.com>
2023-01-12 04:52:18 +0800

    wrapped query param provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,8 @@ import type { AppProps } from 'next/app';
 import { createTheme, ThemeProvider } from '@mui/material';
 import { Inter } from '@next/font/google';
 import Head from 'next/head';
+import { NextAdapter } from 'next-query-params';
+import { QueryParamProvider } from 'use-query-params';
 
 const theme = createTheme({
 	typography: {
@@ -15,18 +17,20 @@ const inter = Inter({ subsets: ['latin'] });
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<ThemeProvider theme={theme}>
-			<Head>
-				<title>RHB Connect</title>
-				<meta
-					name="description"
-					content="RHB connect make it easy for you to contact us for information regarding financial advices or services"
-				/>
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
-			<main className={inter.className}>
-				<Component {...pageProps} />
-			</main>
+			<QueryParamProvider adapter={NextAdapter}>
+				<Head>
+					<title>RHB Connect</title>
+					<meta
+						name="description"
+						content="RHB connect make it easy for you to contact us for information regarding financial advices or services"
+					/>
+					<meta name="viewport" content="width=device-width, initial-scale=1" />
+					<link rel="icon" href="/favicon.ico" />
+				</Head>
+				<main className={inter.className}>
+					<Component {...pageProps} />
+				</main>
+			</QueryParamProvider>
 		</ThemeProvider>
 	);
 }
