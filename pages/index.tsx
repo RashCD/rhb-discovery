7ec4ab568b21fa561c8e9bc9a23d04178a92c7e0
RashCD <mrrash91@gmail.com>
2023-01-12 04:52:41 +0800

    auto redirect to questionnaire page from index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import useEffectOnce from '../hooks/useEffectOnce';
 
 export default function Home() {
-	return (
-		<>
-			<Link href="/questionnaire">Questionnaire</Link>
-		</>
-	);
+	const router = useRouter();
+
+	useEffectOnce(() => {
+		router.push('/questionnaire');
+	});
+
+	return <></>;
 }
