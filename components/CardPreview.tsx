df4ad98c56fbea16f7234232098f4d83284458d9
RashCD <mrrash91@gmail.com>
2023-01-15 05:58:52 +0800

    implement product helper to card preview

--- components/CardPreview.tsx
+++ components/CardPreview.tsx
@@ -2,7 +2,15 @@ import { Box, Grid, Paper, Stack, Typography } from '@mui/material';
 import Image from 'next/image';
 import React from 'react';
 
-const CardPreview = () => {
+const CardPreview = ({
+	title,
+	description,
+	image,
+}: {
+	title: string;
+	description: string;
+	image: string;
+}) => {
 	return (
 		<Grid item md={5} display="flex">
 			<Paper
@@ -14,11 +22,28 @@ const CardPreview = () => {
 					borderRadius: 3,
 					overflow: 'hidden',
 					m: 3,
-					aspectRatio: '1.1/1',
+					aspectRatio: '1/1.1',
 				}}
 			>
 				<Stack width={'100%'} height={'100%'}>
-					<Box sx={{ bgcolor: 'grey', width: '100%', minHeight: 200 }}></Box>
+					<Box
+						sx={{
+							bgcolor: 'grey',
+							width: '100%',
+							minHeight: 200,
+							position: 'relative',
+						}}
+					>
+						<Image
+							alt="product placement"
+							src={image}
+							fill
+							sizes="200px"
+							style={{
+								objectFit: 'cover',
+							}}
+						/>
+					</Box>
 					<Box
 						sx={{
 							height: '100%',
@@ -26,7 +51,12 @@ const CardPreview = () => {
 							py: 2,
 						}}
 					>
-						<Typography>RM10 untuk permulaan pelaburan di ASNB</Typography>
+						<Typography variant="h6" sx={{ mb: 2, fontWeight: '500' }}>
+							{title}
+						</Typography>
+						<Typography variant="subtitle2" sx={{ color: 'grey' }}>
+							{description}
+						</Typography>
 					</Box>
 				</Stack>
 			</Paper>
