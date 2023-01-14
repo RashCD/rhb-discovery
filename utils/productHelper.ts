66ab343b25d575762abd41576c34386890520959
RashCD <mrrash91@gmail.com>
2023-01-15 05:58:37 +0800

    add product helper details with image

---
+++ utils/productHelper.ts
@@ -0,0 +1,30 @@
+export const productHelper = [
+	{
+		title: 'RHB Essential FlexiLink Plus',
+		description:
+			'The whole life investment-linked plan offers a combination of insurance protection and investment. With as little premium as RM1,200 per year..',
+		image: '/flexi.png',
+	},
+	{
+		title: 'RHB Rewards Motion Code',
+		description: 'Min. income RM24,000 per annum',
+		image: '/visa.png',
+	},
+	{
+		title: 'ASB Financing',
+		description:
+			'Maximise your future savings with ASB Financing! ASB units can be yours with high margin of financing, low financing rates and flexible financing tenures.',
+		image: '/financing.png',
+	},
+	{
+		title: 'Unit Trust',
+		description:
+			'The dynamic world of investments can be daunting. As your investments grow bigger, risks management becomes even more crucial. ',
+		image: '/unit.png',
+	},
+	{
+		title: 'RHB MaxSave Account',
+		description: 'Stay safe and keep your money secure with this online banking experience.',
+		image: '/maxsave.png',
+	},
+];
