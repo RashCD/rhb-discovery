20d7d7a6cd237b7ff1c73a55b83553196d401e92
RashCD <mrrash91@gmail.com>
2023-01-15 03:51:57 +0800

    add time picker component

---
+++ components/Time.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import dayjs, { Dayjs } from 'dayjs';
+import TextField from '@mui/material/TextField';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { StaticTimePicker } from '@mui/x-date-pickers/StaticTimePicker';
+
+export default function Time() {
+	const [value, setValue] = React.useState<Dayjs | null>(dayjs('2023-01-01'));
+
+	return (
+		<LocalizationProvider dateAdapter={AdapterDayjs}>
+			<StaticTimePicker
+				displayStaticWrapperAs="mobile"
+				minutesStep={30}
+				minTime={dayjs('2023-01-01T09:00+08:00')}
+				maxTime={dayjs('2023-01-01T18:00+08:00')}
+				value={value}
+				componentsProps={{
+					actionBar: {
+						actions: [],
+					},
+				}}
+				onChange={(newValue) => {
+					setValue(newValue);
+				}}
+				renderInput={(params) => <TextField {...params} />}
+			/>
+		</LocalizationProvider>
+	);
+}
