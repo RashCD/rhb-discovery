c453acb1a235e925e89ccc6c7661a9121bdf426d
RashCD <mrrash91@gmail.com>
2023-01-15 04:18:21 +0800

    remove unused code

--- components/Time.tsx
+++ components/Time.tsx
@@ -1,12 +1,12 @@
-import * as React from 'react';
 import dayjs, { Dayjs } from 'dayjs';
 import TextField from '@mui/material/TextField';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { StaticTimePicker } from '@mui/x-date-pickers/StaticTimePicker';
+import { useState } from 'react';
 
 export default function Time() {
-	const [value, setValue] = React.useState<Dayjs | null>(dayjs('2023-01-01'));
+	const [value, setValue] = useState<Dayjs | null>(dayjs('2023-01-01'));
 
 	return (
 		<LocalizationProvider dateAdapter={AdapterDayjs}>
