931f1fe9aab7112bef2670db5ccd84e0aa4051cb
RashCD <mrrash91@gmail.com>
2023-01-15 03:53:26 +0800

    implement book appointment modal

--- pages/book-session.tsx
+++ pages/book-session.tsx
@@ -1,8 +1,26 @@
-import { Container, Grid, Typography } from '@mui/material';
+import {
+	Avatar,
+	Badge,
+	Button,
+	Container,
+	Grid,
+	Stack,
+	styled,
+	TextField,
+	Typography,
+} from '@mui/material';
 import SessionCard from '../components/SessionCard';
+import Stepper from '../components/Stepper';
+import TransitionModal from '../components/TransitionModal';
 import { sessionHelper } from '../utils/sessionHelper';
+import { useState } from 'react';
+import dayjs, { Dayjs } from 'dayjs';
+import BookingAppointmentContent from '../components/BookingAppointmentContent';
 
 const BookSession = () => {
+	const [value, setValue] = useState<Dayjs | null>(dayjs());
+	const [step, setStep] = useState(0);
+
 	return (
 		<Container sx={{ my: 3, height: '100%' }}>
 			<Typography variant="h4" fontWeight={'500'}>
@@ -13,6 +31,16 @@ const BookSession = () => {
 					<SessionCard key={sessionProfile.name} {...sessionProfile} />
 				))}
 			</Grid>
+			<TransitionModal
+				content={
+					<BookingAppointmentContent
+						step={step}
+						value={value}
+						setValue={setValue}
+						setStep={setStep}
+					/>
+				}
+			/>
 		</Container>
 	);
 };
