11239425258c0c7f948a26476c76cc7892339d57
RashCD <mrrash91@gmail.com>
2023-01-25 03:09:06 +0800

    add a field type into product helper

--- utils/productHelper.ts
+++ utils/productHelper.ts
@@ -5,12 +5,14 @@ export const productHelper = [
 			'The whole life investment-linked plan offers a combination of insurance protection and investment. With as little premium as RM1,200 per year..',
 		image: '/flexi.png',
 		link: 'https://www.rhbgroup.com/personal/life-protection/life-starters/index.html',
+		field: 'Insurance',
 	},
 	{
 		title: 'RHB Rewards Motion Code',
 		description: 'Min. income RM24,000 per annum',
 		image: '/visa.png',
 		link: 'https://www.rhbgroup.com/personal/cards/credit-cards/rhb-rewards-motion-code-credit-card.html',
+		field: 'Credit Card',
 	},
 	{
 		title: 'ASB Financing',
@@ -18,6 +20,7 @@ export const productHelper = [
 			'Maximise your future savings with ASB Financing! ASB units can be yours with high margin of financing, low financing rates and flexible financing tenures.',
 		image: '/financing.png',
 		link: 'https://www.google.com',
+		field: 'Saving',
 	},
 	{
 		title: 'Unit Trust',
@@ -25,10 +28,12 @@ export const productHelper = [
 			'The dynamic world of investments can be daunting. As your investments grow bigger, risks management becomes even more crucial. ',
 		image: '/unit.png',
 		link: 'https://www.google.com',
+		field: 'Investment',
 	},
 	{
 		title: 'RHB MaxSave Account',
 		description: 'Stay safe and keep your money secure with this online banking experience.',
 		image: '/maxsave.png',
+		field: 'Investment',
 	},
 ];
