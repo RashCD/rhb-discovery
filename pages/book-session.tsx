f6f9f8741c34eeb3b59029565b4f59f98a1dca9c
RashCD <mrrash91@gmail.com>
2023-01-26 01:20:41 +0800

    enable filter feature

--- pages/book-session.tsx
+++ pages/book-session.tsx
@@ -1,15 +1,19 @@
-import { Container, Grid, Typography } from '@mui/material';
+import { Container, Grid, SelectChangeEvent, Stack, Typography } from '@mui/material';
 import SessionCard from '../components/SessionCard';
 import TransitionModal from '../components/TransitionModal';
-import { sessionHelper } from '../utils/sessionHelper';
+import { FieldType, LanguageType, sessionHelper } from '../utils/sessionHelper';
 import { useState } from 'react';
 import BookingAppointmentContent from '../components/BookingAppointmentContent';
 import { StringParam, useQueryParam, withDefault } from 'use-query-params';
+import FilterSelect from '../components/FilterSelect';
 
 const BookSession = () => {
 	const [step, setStep] = useState(0);
 	const [open, setOpen] = useState(false);
 	const [account, setAccount] = useState({ name: '', image: '' });
+	const [location, setLocation] = useState('');
+	const [fields, setFields] = useState('');
+	const [language, setLanguage] = useState('');
 
 	const [instructor, setInstructor] = useQueryParam('instructor', withDefault(StringParam, ''));
 
@@ -24,13 +28,102 @@ const BookSession = () => {
 
 	const handleClose = () => setOpen(false);
 
+	const handleFieldsOnChange = (event: SelectChangeEvent<string>) =>
+		setFields(event.target.value);
+
+	const handleLocationOnChange = (event: SelectChangeEvent<string>) =>
+		setLocation(event.target.value);
+
+	const handleLanguageOnChange = (event: SelectChangeEvent<string>) =>
+		setLanguage(event.target.value);
+
+	// capitalize first letter of string eg: 'saving' => 'Saving' & 'kuala lumpur' => 'Kuala Lumpur'
+	const capitalize = (str: string) => {
+		const splitStr = str.toLowerCase().split(' ');
+		if (splitStr.length > 1) {
+			return splitStr
+				.map((word) => word.charAt(0).toUpperCase() + word.substring(1))
+				.join(' ');
+		} else {
+			return str.charAt(0).toUpperCase() + str.substring(1);
+		}
+	};
+
+	// filter sessionHelper by fields, location, and language
+	const filteredSessions = sessionHelper
+		.filter((session) => {
+			if (fields) {
+				return session.fields.includes(capitalize(fields) as FieldType);
+			} else {
+				return true;
+			}
+		})
+		.filter((session) => {
+			if (location) {
+				return session.location.includes(capitalize(location));
+			} else {
+				return true;
+			}
+		})
+		.filter((session) => {
+			if (language) {
+				return session.language.includes(capitalize(language) as LanguageType);
+			} else {
+				return true;
+			}
+		});
+
 	return (
 		<Container sx={{ my: 3, height: '100%' }}>
-			<Typography variant="h4" fontWeight={'500'}>
+			<Typography variant="h4" fontWeight={'500'} sx={{ mb: 3 }}>
 				Talk to our officers
 			</Typography>
+			<Stack
+				direction={['column', 'row']}
+				gap={2}
+				sx={{ margin: 'auto', width: ['90%', '90%', '70%'] }}
+			>
+				<FilterSelect
+					label="Fields"
+					options={[
+						{ value: '', label: 'None' },
+						{ value: 'saving', label: 'Saving' },
+						{ value: 'insurance', label: 'Insurance' },
+						{ value: 'loan', label: 'Loan' },
+						{ value: 'investment', label: 'Investment' },
+					]}
+					value={fields}
+					onChange={handleFieldsOnChange}
+				/>
+				<FilterSelect
+					label="Location"
+					options={[
+						{ value: '', label: 'None' },
+						{ value: 'kuala lumpur', label: 'Kuala Lumpur' },
+						{ value: 'selangor', label: 'Selangor' },
+						{ value: 'johore', label: 'Johore' },
+						{ value: 'pahang', label: 'Pahang' },
+						{ value: 'sarawak', label: 'Sarawak' },
+						{ value: 'negeri sembilan', label: 'Negeri Sembilan' },
+					]}
+					value={location}
+					onChange={handleLocationOnChange}
+				/>
+				<FilterSelect
+					label="Language"
+					options={[
+						{ value: '', label: 'None' },
+						{ value: 'malay', label: 'Malay' },
+						{ value: 'english', label: 'English' },
+						{ value: 'mandarin', label: 'Mandarin' },
+						{ value: 'tamil', label: 'Tamil' },
+					]}
+					value={language}
+					onChange={handleLanguageOnChange}
+				/>
+			</Stack>
 			<Grid container my={3}>
-				{sessionHelper.map((sessionProfile) => (
+				{filteredSessions.map((sessionProfile) => (
 					<SessionCard
 						key={sessionProfile.name}
 						handleOpen={handleOpen}
