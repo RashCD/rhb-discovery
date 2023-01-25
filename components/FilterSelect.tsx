1e42a6cd0acc85d277f07dccbfb6c7bc051a89dc
RashCD <mrrash91@gmail.com>
2023-01-26 01:45:27 +0800

    add size props to filter select component

--- components/FilterSelect.tsx
+++ components/FilterSelect.tsx
@@ -3,13 +3,20 @@ import { FormControl, InputLabel, Select, MenuItem, SelectChangeEvent } from '@m
 type FilterSelectProps = {
 	label: string;
 	value?: string;
+	size?: 'small' | 'medium';
 	options: { value: string; label: string }[];
 	onChange: (event: SelectChangeEvent<string>, child: React.ReactNode) => void;
 };
 
-const FilterSelect = ({ label, value = '', onChange, options }: FilterSelectProps) => {
+const FilterSelect = ({
+	label,
+	value = '',
+	size = 'small',
+	onChange,
+	options,
+}: FilterSelectProps) => {
 	return (
-		<FormControl fullWidth size="small">
+		<FormControl fullWidth size={size}>
 			<InputLabel id={`${label}-select-label`}>{label}</InputLabel>
 			<Select
 				labelId={`${label}-select-label`}
