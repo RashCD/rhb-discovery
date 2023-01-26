b64377fb0501f452848703df08127daef5001a1f
RashCD <mrrash91@gmail.com>
2023-01-27 01:39:25 +0800

    add og tag for facebook & twitter

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -67,7 +67,10 @@ export default function App({ Component, pageProps }: AppProps) {
 						/>
 						<meta name="viewport" content="width=device-width, initial-scale=1" />
 						<link rel="icon" href="/faviconrhb.png" />
-						<meta property="og:site_name" content="RHB Discovery" />
+
+						{/* Facebook Meta Tags */}
+						<meta property="og:url" content="https://rhb-discovery.vercel.app/" />
+						<meta property="og:type" content="website" />
 						<meta property="og:title" content="RHB Discovery" />
 						<meta
 							property="og:description"
@@ -77,11 +80,20 @@ export default function App({ Component, pageProps }: AppProps) {
 							property="og:image"
 							content="https://rhb-discovery.vercel.app/landing_og.jpeg"
 						/>
-						<meta property="og:url" content="https://rhb-discovery.vercel.app" />
-						<meta property="og:type" content="website" />
-						<meta property="og:image:type" content="image/jpeg" />
-						<meta property="og:image:width" content="300" />
-						<meta property="og:image:height" content="200" />
+
+						{/* Twitter Meta Tags  */}
+						<meta name="twitter:card" content="summary_large_image" />
+						<meta property="twitter:domain" content="rhb-discovery.vercel.app" />
+						<meta property="twitter:url" content="https://rhb-discovery.vercel.app/" />
+						<meta name="twitter:title" content="RHB Discovery" />
+						<meta
+							name="twitter:description"
+							content="RHB Discovery make it easy for you to contact us for information regarding financial advices or services"
+						/>
+						<meta
+							name="twitter:image"
+							content="https://rhb-discovery.vercel.app/landing_og.jpeg"
+						/>
 					</Head>
 					<main>
 						<Component {...pageProps} />
