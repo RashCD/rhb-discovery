a228a825b842c8070a6e663811b107c6d6a6348f
RashCD <mrrash91@gmail.com>
2023-01-28 00:55:44 +0800

    add dashboard lead page

---
+++ pages/dashboard/lead.tsx
@@ -0,0 +1,26 @@
+import { Box, Stack, Typography } from '@mui/material';
+import DashboardLayout from '../../components/DashboardLayout';
+
+const DashboardList = () => {
+	return (
+		<Stack direction={'row'}>
+			<DashboardLayout />
+			<Stack sx={{ ml: '270px', width: '100%' }}>
+				<Typography component={'h2'} variant="h4" fontWeight={'bold'} sx={{ my: 3 }}>
+					RHB Discovery Leads
+				</Typography>
+				<Box
+					sx={{
+						backgroundImage: 'url(/dashboard-lead.png)',
+						backgroundRepeat: 'no-repeat',
+						backgroundSize: 'contain',
+						aspectRatio: '1.01',
+						scale: '0.8',
+					}}
+				/>
+			</Stack>
+		</Stack>
+	);
+};
+
+export default DashboardList;
