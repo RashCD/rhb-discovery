4b3fd4b24a001f5430593bf2f6bd243dd96ecdb5
RashCD <mrrash91@gmail.com>
2023-01-27 19:10:51 +0800

    update carousel logic

--- pages/fitness-type.tsx
+++ pages/fitness-type.tsx
@@ -1,12 +1,22 @@
-import { Button, Container, Box, Stack, Typography } from '@mui/material';
+import { Button, Container, Box, Stack, Typography, TextField } from '@mui/material';
 import Image from 'next/image';
-import { useState } from 'react';
+import { SetStateAction, useState } from 'react';
 import Carousel from 'react-material-ui-carousel';
 import FinancialDescription from '../components/FinancialDescription';
+import FinancialQuizStarter from '../components/FinancialQuizStarter';
 import FinancialTypes from '../components/FinancialTypes';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import { useRouter } from 'next/router';
 
 const FitnessType = () => {
 	const [carouselIndex, setCarouselIndex] = useState(0);
+	const [userEmail, setUserEmail] = useState('');
+
+	const router = useRouter();
+
+	const handleEmailChange = (event: { target: { value: SetStateAction<string> } }) => {
+		setUserEmail(event.target.value);
+	};
 
 	return (
 		<Container sx={{ my: 5, height: '100%' }}>
@@ -42,17 +52,39 @@ const FitnessType = () => {
 					}}
 					next={(index) => index && setCarouselIndex(index)}
 					prev={() => setCarouselIndex((prev) => prev - 1)}
+					onChange={(index) => index && setCarouselIndex(index)}
 				>
 					<FinancialTypes />
 					<FinancialDescription />
+					<FinancialQuizStarter />
 				</Carousel>
-				<Button
-					variant="contained"
-					sx={{ bgcolor: 'tertiary.main', borderRadius: 2, width: 200, mt: 3 }}
-					onClick={() => setCarouselIndex(carouselIndex + 1)}
-				>
-					Next
-				</Button>
+				{carouselIndex === 2 ? (
+					<>
+						<TextField
+							label="Share Your Email"
+							variant="outlined"
+							onChange={handleEmailChange}
+							value={userEmail}
+						/>
+						<Button
+							variant="contained"
+							endIcon={<ArrowForwardIosIcon />}
+							sx={{ bgcolor: 'tertiary.main', borderRadius: 2, width: 200, mt: 3 }}
+							disabled={userEmail === ''}
+							onClick={() => router.push('/questionnaire')}
+						>
+							Start the Quiz
+						</Button>
+					</>
+				) : (
+					<Button
+						variant="contained"
+						sx={{ bgcolor: 'tertiary.main', borderRadius: 2, width: 200, mt: 3 }}
+						onClick={() => setCarouselIndex(carouselIndex + 1)}
+					>
+						Next
+					</Button>
+				)}
 			</Stack>
 		</Container>
 	);
