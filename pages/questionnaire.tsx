9edf212e64940708d7d0ace5fd74c5df376ae6f2
RashCD <mrrash91@gmail.com>
2023-01-27 19:12:06 +0800

    add rhb logo to the top

--- pages/questionnaire.tsx
+++ pages/questionnaire.tsx
@@ -1,8 +1,9 @@
-import { Container } from '@mui/material';
+import { Box, Container } from '@mui/material';
 import Question from '../components/Question';
 import { useQueryParam, NumberParam } from 'use-query-params';
 import useEffectOnce from '../hooks/useEffectOnce';
 import { useRouter } from 'next/router';
+import Image from 'next/image';
 
 export type QuestionTypes = {
 	q: string;
@@ -21,7 +22,18 @@ const Questionnaire = () => {
 	});
 
 	return (
-		<Container sx={{ mt: 10 }}>
+		<Container sx={{ mt: 3 }}>
+			<Box sx={{ mb: 7, display: 'flex', justifyContent: 'center' }}>
+				<Image
+					alt="rhb discovery logo"
+					src={'/discovery.png'}
+					width={400}
+					height={50}
+					style={{
+						objectFit: 'contain',
+					}}
+				/>
+			</Box>
 			<Question />
 		</Container>
 	);
